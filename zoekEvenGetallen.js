7482f0264f802112b425c6d86c96bffff7740e3b
Rogier <rogiertenhacken@gmail.com>
2018-01-31 08:50:35 +0100

    Update zoekEvenGetallen.js

--- zoekEvenGetallen.js
+++ zoekEvenGetallen.js
@@ -1,10 +1,9 @@
 const numbers = [1,2,3,5,7,6,9,4,8,0]
 
   const evenNumbers = numbers.filter(number => {
-    //console.log(number, ' gaat door de filter functie heen')
     if(number % 2 === 0) {
       return number
     }
-})
+});
 
 console.log(evenNumbers);
